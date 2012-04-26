21fd9a6a231d44eb2cf74b723cfa8096c5fc6116
Andrew Reed <andrew.reed@west-point.org>
2012-04-26 13:31:43 -0700

    Added KeyshancRT functions.

--- javascript/keyshanc.js
+++ javascript/keyshanc.js
@@ -24,11 +24,18 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-//NOTE: This script requires sha512-min.js and sha256-min.js
-//Both can be obtained from http://pajhome.org.uk/crypt/md5/jshash-2.2.zip
+//NOTE: This script requires sha512-min.js, sha256-min.js, and sha1-min.js
+//They can be obtained from http://pajhome.org.uk/crypt/md5/jshash-2.2.zip
 
 //ALSO: The dynamicTweet/generateTweet portions require jQuery
 //Available from http://jquery.com
+
+//ALSO: KeyshancRT requires hotp.js, avail. @ github.com/adulau/hotp-js
+//& 2.0.0-crypto-sha1.js & 2.0.0-hmac-min.js, avail. @ crypto-js.googlecode.com
+
+var minutes = 1000*60;
+var minuteCounter = 0;
+var timestamp = new String("");
 
 //create keys[95]
 var keys = new Array(95);
@@ -89,8 +96,11 @@ function keyshanc(password) {
     }
 }
 
+//Standard decryption function (non Real-Time version)
+//It pulls ciphertext from a textarea named "inText"
+//and outputs plaintext to a textarea named "outText".
 function decryptKeyshanc() {
-    s1 = new String(document.getElementById('inText').value);
+    var s1 = new String(document.getElementById('inText').value);
 
     var s2 = new String("");
 
@@ -114,8 +124,11 @@ function decryptKeyshanc() {
     document.getElementById('outText').value = s2;
 }
 
+//Standard encryption function (non Real-Time version)
+//It pulls plaintext from a textarea named "inText"
+//and outputs ciphertext to a textarea named "outText".
 function encryptKeyshanc() {
-    s1 = new String(document.getElementById('inText').value);
+    var s1 = new String(document.getElementById('inText').value);
 
     var s2 = new String("");
 
@@ -133,11 +146,188 @@ function encryptKeyshanc() {
     document.getElementById('outText').value = s2;
 }
 
+//Decrypt a message that was created using Keyshanc Real-Time
+//It pulls an encrypted message from a textarea named "encryptedText"
+//and outputs the decrypted message to a textarea named "decryptionOutput".
+function decryptKeyshancRT(password) {
+    var encryptedString = new String(document.getElementById('encryptedText').value);
+
+    //locate the delimiter ¥
+    var findYen = encryptedString.indexOf('¥');
+
+    //everything to the right of the timestamp is assumed to be encrypted text
+    var s1 = new String(encryptedString.substring((findYen + 5)));
+
+    //the timestamp is comprised of the 4 characters to the right of ¥
+    var encryptedTimestamp = new String(encryptedString.substring((findYen + 1), (findYen + 5)));
+
+    //convert the timestamp back into the number of minutes since the Unix epoch
+    var decryptedTimestamp = convertTimestamp(encryptedTimestamp);
+
+    //recompute Keyshanc based on the timestamp from the message
+    var seed = createSeed(password);
+    var pin = hotp(seed,decryptedTimestamp,"dec8");
+    keyshanc(password.concat(pin));
+
+    var s2 = new String("");
+
+    for (x = 0; x < s1.length; x++) {
+        if (s1.charCodeAt(x) >= 32 && s1.charCodeAt(x) <= 126)
+        {
+            for (y = 0; y < 95; y++) {
+                if (s1.charAt(x) == String.fromCharCode(keys[y]))
+                {
+                    s2 = s2.concat(String.fromCharCode(y+32));
+                    break;
+                }
+            }
+        }
+        else
+        {
+            s2 = s2.concat(s1.charAt(x));
+        }
+    }
+
+    //it is assumed that if ¥ is greater than 0, then there is some plaintext
+    //to the left of ¥ that should be prepended to the decrypted message
+    if (findYen > 0)
+    {
+        var plaintext = new String(encryptedString.substring(0, findYen));
+        s2 = plaintext.concat(s2);
+    }
+
+    document.getElementById('decryptionOutput').value = s2;
+}
+
+//Encrypt messages using Keyshanc Real-Time
+//It encrypts the text located in the textarea named "inText".
+//Any text located in the textarea named "plainText" will not be
+//encrypted. The final message is output to the textarea named
+//"outText".
+function encryptKeyshancRT(password) {
+    //update the time with every keystroke
+    //time is based on the minutes since the Unix epoch
+    var date = new Date();
+    var tempCounter = Math.floor((date.getTime())/minutes);
+
+    //if the time has changed, then recompute Keyshanc
+    if (minuteCounter != tempCounter)
+    {
+        minuteCounter = tempCounter;
+
+        //HOTP requires a seed value
+        var seed = createSeed(password);
+
+        //supplying the minuteCounter to the HOTP function
+        //turns it into a TOTP function
+        var pin = hotp(seed,minuteCounter,"dec8");
+
+        //recompute Keyshanc using the (password+pin)
+        //this causes the shuffle to change every minute
+        keyshanc(password.concat(pin));
+    }
+
+    var s1 = new String(document.getElementById('inText').value);
+
+    var s2 = new String("");
+
+    for (x = 0; x < s1.length; x++) {
+        if (s1.charCodeAt(x) >= 32 && s1.charCodeAt(x) <= 126)
+        {
+            s2 = s2.concat(String.fromCharCode(keys[((s1.charCodeAt(x))-32)]));
+        }
+        else
+        {
+            s2 = s2.concat(s1.charAt(x));
+        }
+    }
+
+    //create the timestamp
+    createTimestamp(minuteCounter);
+
+    //prepend the timestamp to the encrypted text
+    s2 = timestamp + s2;
+
+    var plaintext = new String(document.getElementById('plainText').value);
+
+    //if there is plaintext, then prepend it to the encrypted message
+    if (plaintext.length != 0)
+    {
+        s2 = plaintext + " " + s2;
+    }
+
+    document.getElementById('outText').value = s2;
+}
+
+//Use the password to create a seed for the HOTP function.
+//The seed is created by SHA-1 hashing the password and then
+//concatenating the password and hash for another round of SHA-1
+//hashing. (This is how the app "Android Token" creates a seed
+//from a password.)
+function createSeed(password) {
+    var seed = new String(hex_sha1(password));
+    seed = hex_sha1(password.concat(seed.toUpperCase()));
+    return seed.toUpperCase();
+}
+
+//The timestamp is encoded in Base94.
+//I use ASCII characters ! thru ~ as the Base94 "digits".
+//This encoding will work for 148.5 years (until June 2118).
+function createTimestamp(numMinutes) {
+    var power3, power2, power1, power0;
+
+    power3 = numMinutes / (Math.pow(94, 3));
+    numMinutes %= Math.pow(94, 3);
+
+    power2 = numMinutes / (Math.pow(94, 2));
+    numMinutes %= Math.pow(94, 2);
+
+    power1 = numMinutes / (Math.pow(94, 1));
+    numMinutes %= Math.pow(94, 1);
+
+    power0 = numMinutes / (Math.pow(94, 0));
+    numMinutes %= Math.pow(94, 0);
+
+    var char3, char2, char1, char0;
+
+    char3 = String.fromCharCode(power3 + 33);
+    char2 = String.fromCharCode(power2 + 33);
+    char1 = String.fromCharCode(power1 + 33);
+    char0 = String.fromCharCode(power0 + 33);
+
+    //¥ is used as a delimiter for the timestamp.
+    timestamp = '¥' + char3 + char2 + char1 + char0;
+}
+
+function convertTimestamp(messageTimestamp) {
+    var power3 = ((messageTimestamp.charCodeAt(0)) - 33);
+    var power2 = ((messageTimestamp.charCodeAt(1)) - 33);
+    var power1 = ((messageTimestamp.charCodeAt(2)) - 33);
+    var power0 = ((messageTimestamp.charCodeAt(3)) - 33);
+    
+    var messageCounter = 0;
+    messageCounter += ((Math.pow(94, 3)) * power3);
+    messageCounter += ((Math.pow(94, 2)) * power2);
+    messageCounter += ((Math.pow(94, 1)) * power1);
+    messageCounter += ((Math.pow(94, 0)) * power0);
+    return messageCounter;
+}
+
 function generateTweet(tweetText) {
     tweetText = encodeURIComponent(tweetText);
     tweetText = tweetText.replace(/`/g, "%60");
     tweetText = tweetText.replace(/\'/g, "%27");
     $('#dynamicTweet').html("\
 <iframe allowtransparency='true' frameborder='0' scrolling='no' src='http://platform.twitter.com/widgets/tweet_button.html?text=" + tweetText + "%20decrypt%20at%20http%3A%2F%2Fandrewcreed.com%2F2012%2F04%2F16%2Fkeyshanc-ported-to-javascript.html%20encrypt%20at&hashtags=keyshanc&count=none' style='width:115px; height:21px;'></iframe>\
+<br />");
+}
+
+//This Tweet button doesn't have the extra URL & hashtag.
+function generateTweetRT(tweetText) {
+    tweetText = encodeURIComponent(tweetText);
+    tweetText = tweetText.replace(/`/g, "%60");
+    tweetText = tweetText.replace(/\'/g, "%27");
+    $('#dynamicTweet').html("\
+<iframe allowtransparency='true' frameborder='0' scrolling='no' src='http://platform.twitter.com/widgets/tweet_button.html?text=" + tweetText + "&count=none' style='width:115px; height:21px;'></iframe>\
 <br />");
 }
